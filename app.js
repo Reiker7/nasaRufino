b98ff8cc87cdb4f34e86a795541fe335e8d65442
Reiker <reiker1@hotmail.com>
2022-11-28 20:44:29 +0100

    endpointsOperativos

--- app.js
+++ app.js
@@ -3,29 +3,77 @@ const mongoose = require('mongoose');
 const app = express()
 
 app.use(express.json())
-app.use(function(req, res, next){
-    console.log("lets go")
-    next()
-})
+const landingsSchema = new mongoose.Schema({
+    name:  String, 
+    id: String,
+    nametype: String,
+    reclass:   String,
+    mass: String,
+    fall: String,
+    year: String,
+    reclat: String,
+    id_deg: String,
+    reclong: String,
+    geolocatipon: String,
+  });
+const neasSchema = new mongoose.Schema({
+    designation:  String, 
+    price: String,
+    discovery_date: Date,
+    h_mag:   String,
+    moid_au: String,
+    q_au_1: String,
+    q_au_2: String,
+    period_ys: String,
+    id_deg: String,
+    pha: String,
+    orbit_class: String,
+  });
+
+const Landings = mongoose.model('landings', landingsSchema)
+const Neas = mongoose.model('neas', neasSchema)
+
 async function connection() {
     try {
-      await mongoose.connect('mongodb://127.0.0.1:27017/api')
+      await mongoose.connect('mongodb://127.0.0.1:27017/nasa')
     } 
     catch (err) {
-        console.log("no vamos", err)
+        console.log("keep calm", err)
     }
 }
+    //?minimum_mass=200000
+    app.get('/api/astronomy/landings', async (req,res) =>{
+        await connection()
+        let Qsulta = req.query
+        console.log(Qsulta.minimum_mass)
 
-app.get('/api/entries', (req,res) =>{
-    // console.log("solicitud recibida");
-    pool.query('SELECT title, content, date_, category, name_, surname, image FROM authors AS A INNER JOIN entries AS E ON A.id_author = E.id_author',(err,rows)=>{
-        if(err) return res.status(500).send("Keep calm")
-        res.json(rows)
+        const result = await Landings.find({mass:{$gt: Qsulta.minimum_mass}})
+        // .sort('price title')
+        .select('price mass')
+        
+    
+    
+        // console.log('landings')
+        res.send(result)
+        
     })
-})
+    app.get('/api/astronomy/neas', async (req,res) =>{
+        await connection()
 
 
-connection()
+        const result = await Neas.find({})
+
+
+        // console.log('neas')
+        res.send(result)
+        
+    })
+
+
+
+
+
+
 app.listen(3000, () => {
     console.log("Server on 3000");
 })
